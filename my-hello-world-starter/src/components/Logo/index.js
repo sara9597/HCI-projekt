0c5ca4fd19195a1d28b35109c6ade905c05f7867
SJ <sjukic00@fesb.hr>
2021-01-14 10:29:44 +0100

    Adding Logo

--- my-hello-world-starter/src/components/Logo/index.js
+++ my-hello-world-starter/src/components/Logo/index.js
@@ -1,6 +1,13 @@
 import React from "react"
 import styles from "./style.module.css"
 
-const Logo = () => <div className={styles.logo}></div>
+const Logo = () => (
+  <div className={styles.logo}>
+    <img
+      src="https://github.com/sara9597/HCI-projekt/blob/master/my-hello-world-starter/src/images/Logo.png?raw=true"
+      className={styles.icon}
+    ></img>
+  </div>
+)
 
 export default Logo
