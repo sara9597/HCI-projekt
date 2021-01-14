33330820e2ac7145c157293089fd47314e312558
SJ <sjukic00@fesb.hr>
2021-01-14 14:23:40 +0100

    About us

---
+++ my-hello-world-starter/src/components/AboutUs/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import styles from "./style.module.css"
+
+const AboutUs = () => (
+  <section className={styles.about}>
+    <p>
+      Trying to find the perfect musician for your event?
+      <strong> MusicAgent</strong> is the right place for you. <br /> Connect
+      with over <strong> 1,000 </strong>musicians with just a few clicks.
+      <br /> MusicAgent has been connecting clients and musicians since 2020.{" "}
+      <br />
+      <br />
+      If you are a musician and want to be a part of our community
+      <strong> JOIN </strong> us!
+    </p>
+  </section>
+)
+
+export default AboutUs
--- my-hello-world-starter/src/pages/index.js
+++ my-hello-world-starter/src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 
 import HeaderFooterLayout from "../layouts/headerFooter"
 import TitlePage from "../modules/TitlePage"
+import AboutUs from "../components/AboutUs"
 
 const IndexPage = () => (
   <HeaderFooterLayout activeTab="Home">
     <TitlePage />
+    <AboutUs />
   </HeaderFooterLayout>
 )
 
