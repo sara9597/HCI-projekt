bd1289fb5ecdbe7f4239421a780c18da592f497b
SJ <sjukic00@fesb.hr>
2021-01-18 15:24:17 +0100

    Clickable Logo

--- my-hello-world-starter/src/components/Logo/index.js
+++ my-hello-world-starter/src/components/Logo/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import styles from "./style.module.css"
+import { Link } from "react-router"
 
 const Logo = () => (
   <div className={styles.logo}>
     <img
       src="https://github.com/sara9597/HCI-projekt/blob/master/my-hello-world-starter/src/images/Logo.png?raw=true"
       className={styles.icon}
+      onClick={() => window.open("/", "_self")}
     ></img>
   </div>
 )
