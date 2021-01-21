7beecbee6ea50cb9a6569b7e2054f03a36c6c836
kknezovic <karla.knezovic@gmail.com>
2021-01-21 23:55:00 +0100

    Home BlogParagraph

--- my-hello-world-starter/src/components/BlogParagraph/index.js
+++ my-hello-world-starter/src/components/BlogParagraph/index.js
@@ -1,23 +1,3 @@
-// import React from "react"
-// import styles from "./style.module.css"
-
-// const BlogParagraph = ({ image, title, text }) => (
-//   <div className={styles.imageParagraph}>
-//     <div className={styles.imageHalf}>
-//       {image}
-//       <div className={styles.articleHalf}>
-//         <article>
-//           <h2>{title}</h2>
-//           <p>{text}</p>
-//           <button className={styles.button1}>See more</button>
-//         </article>
-//       </div>
-//     </div>
-//   </div>
-// )
-
-// export default BlogParagraph
-
 import React from "react"
 import styles from "./style.module.css"
 
@@ -29,7 +9,9 @@ const BlogParagraph = ({ image, title, text }) => (
         <article>
           <h2>{title}</h2>
           <p>{text}</p>
-          <button className={styles.button1}>See more</button>
+          <section>
+            <button className={styles.button1}>See more</button>
+          </section>
         </article>
       </div>
     </div>
