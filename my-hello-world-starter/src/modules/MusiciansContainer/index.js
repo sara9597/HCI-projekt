5365debdeab0dca39e5bb84a890754f31359c251
SJ <sjukic00@fesb.hr>
2021-02-11 12:38:24 +0100

    Implementing search filter

--- my-hello-world-starter/src/modules/MusiciansContainer/index.js
+++ my-hello-world-starter/src/modules/MusiciansContainer/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import Img from "gatsby-image"
 import styles from "./style.module.css"
@@ -17,7 +17,7 @@ const MusiciansContainer = () => {
           slug
           updatedAt
           image {
-            fixed(width: 380, height: 300) {
+            fixed(width: 280, height: 200) {
               src
               srcSet
               srcSetWebp
@@ -33,32 +33,71 @@ const MusiciansContainer = () => {
     }
   `)
 
+  const allMusicians = data.myQuery.nodes
+
+  const [state, setState] = useState({
+    filteredMusicians: [],
+    query: ""
+  })
+
+  const handleInputChange = event => {
+    const query = event.target.value
+    const filteredMusicians = allMusicians.filter(post => {
+      const { title, description } = post
+      return (
+        description.toLowerCase().includes(query.toLowerCase()) ||
+        title.toLowerCase().includes(query.toLowerCase())
+      )
+    })
+
+    setState({
+      query,
+      filteredMusicians
+    })
+  }
+
+  const posts = data.myQuery.nodes ? state.filteredMusicians : allMusicians
+
   return (
-    <section className={styles.imageParagraph}>
-      <ul className={styles.list}>
-        {data.myQuery.nodes.map(node => {
-          return (
-            <Link to={`/post/${node.slug}`}>
-              <section className={styles.imageParagraph}>
-                <li>
-                  <div className={styles.imageHalf}>
-                    <Img fixed={node.image.fixed} />
-                    <div className={styles.articleHalf}>
-                      <article>
-                        <h2>{node.title}</h2>
-                        <p>{node.description}</p>
-                        <button className={styles.button1}>View profile</button>
-                      </article>
+    <>
+      <div className={styles.search}>
+        <input
+          className={styles.input}
+          type="text"
+          aria-label="Search"
+          placeholder="Search musicians"
+          value={state.query}
+          onChange={handleInputChange}
+        />{" "}
+      </div>
+      <section className={styles.imageParagraph}>
+        <ul className={styles.list}>
+          {posts.map(node => {
+            return (
+              <Link to={`/post/${node.slug}`}>
+                <section className={styles.imageParagraph}>
+                  <li>
+                    <div className={styles.imageHalf}>
+                      <Img fixed={node.image.fixed} />
+                      <div className={styles.articleHalf}>
+                        <article>
+                          <h2>{node.title}</h2>
+                          <p>{node.description}</p>
+                          <button className={styles.button1}>
+                            View profile
+                          </button>
+                        </article>
+                      </div>
                     </div>
-                  </div>
-                  <span>{node.internal.content}</span>
-                </li>
-              </section>
-            </Link>
-          )
-        })}
-      </ul>
-    </section>
+                    <span>{node.internal.content}</span>
+                  </li>
+                </section>
+              </Link>
+            )
+          })}
+        </ul>
+      </section>
+    </>
   )
 }
 
--- my-hello-world-starter/src/pages/musicians.js
+++ my-hello-world-starter/src/pages/musicians.js
@@ -2,10 +2,10 @@ import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
 import MusiciansContainer from "../modules/MusiciansContainer"
 import Pages from "../components/Pages"
-import Search from "../components/Search"
 
 const MusiciansPage = () => (
     <HeaderFooterLayout activeTab="MUSICIANS">
+    
     <MusiciansContainer />
     <Pages/>
     </HeaderFooterLayout>
