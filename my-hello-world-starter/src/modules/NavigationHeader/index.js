0f8c98990be3215f5d3ebbfe69ba6a317c2bb72e
kknezovic <karla.knezovic@gmail.com>
2021-02-13 04:16:06 +0100

    Responsive header

--- my-hello-world-starter/src/modules/NavigationHeader/index.js
+++ my-hello-world-starter/src/modules/NavigationHeader/index.js
@@ -5,9 +5,11 @@ import Navigation from "../../components/Navigation"
 
 import styles from "./style.module.css"
 import LoginButton from "../../components/LoginButton"
+import MenuIcon from "@material-ui/icons/Menu"
 
 const NavigationHeader = ({ activeTab }) => (
   <section className={styles.navigationHeader}>
+    <div className={styles.menuIcon}><MenuIcon /></div>
     <Logo />
     <Navigation activeTab={activeTab} />
     <LoginButton />
