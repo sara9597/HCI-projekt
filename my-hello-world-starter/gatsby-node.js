4e7c51b149cad17eb620de7ba460c5bfda4b78fd
kknezovic <karla.knezovic@gmail.com>
2021-02-14 23:20:21 +0100

    Dynamic page creation for Musicians

--- my-hello-world-starter/gatsby-node.js
+++ my-hello-world-starter/gatsby-node.js
@@ -43,53 +43,52 @@ exports.createPages = async ({ graphql, actions }) => {
     path: `post/${e.slug}`,
     slug: `post/${e.slug}`
   }))
+
+
+    const ram = await graphql(`
+      query {
+        allContentfulMusicianBox {
+          nodes {
+            internal {
+              content
+            }
+  
+            title
+            description
+            slug
+            updatedAt
+            image {
+              fixed(width: 280, height: 200) {
+                src
+                srcSet
+                srcSetWebp
+                srcWebp
+                width
+                height
+                base64
+                aspectRatio
+              }
+            }
+          }
+        }
+      }
+    `)
+  
+    const rem = ram.data.allContentfulMusicianBox.nodes
+  
+    rem.forEach((e, index, array) =>
+      actions.createPage({
+        component: path.resolve(`./src/Layouts/musician.js`),
+        context: {
+          ...e,
+          next: index < array.length ? array[index + 1] : null,
+          prev: index > 0 ? array[index - 1] : null
+        },
+        path: `post/${e.slug}`,
+        slug: `post/${e.slug}`
+      })
+    )
+    
+
 }
 
-
-// exports.createPages = async ({ graphql, actions }) => {
-//   const raw = await graphql(`
-//     query {
-//       allContentfulMusicianBox {
-//         nodes {
-//           internal {
-//             content
-//           }
-
-//           title
-//           description
-//           slug
-//           updatedAt
-//           image {
-//             fixed(width: 280, height: 200) {
-//               src
-//               srcSet
-//               srcSetWebp
-//               srcWebp
-//               width
-//               height
-//               base64
-//               aspectRatio
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `)
-
-//   const res = raw.data.allContentfulMusicianBox.nodes
-
-//   res.forEach((e, index, array) =>
-//     actions.createPage({
-//       component: path.resolve(`./src/Layouts/musician.js`),
-//       context: {
-//         ...e,
-//         next: index < array.length ? array[index + 1] : null,
-//         prev: index > 0 ? array[index - 1] : null
-//       },
-//       path: `musicians/${e.slug}`,
-//       slug: `musicians/${e.slug}`
-//     })
-//   )
-// }
-
- 
--- my-hello-world-starter/src/Layouts/musician.js
+++ my-hello-world-starter/src/Layouts/musician.js
@@ -1,34 +1,34 @@
-// import React from "react"
-// import { renderRichText } from "gatsby-source-contentful/rich-text"
-// import Img from "gatsby-image"
-// import { Link } from "gatsby"
+import React from "react"
+//import { renderRichText } from "gatsby-source-contentful/rich-text"
+import Img from "gatsby-image"
+import { Link } from "gatsby"
 
-// import styles from "./musician.module.css"
-// import HeaderFooterLayout from "../layouts/headerFooter"
+import styles from "./musician.module.css"
+import HeaderFooterLayout from "../layouts/headerFooter"
 
-// const MusicianPost = ({ pageContext }) => {
-//   const { title, image, next, prev } = pageContext
+const MusicianPost = ({ pageContext }) => {
+  const { title, image, next, prev } = pageContext
 
-//   return (
-//     <HeaderFooterLayout>
-//       <main className={styles.container}>
-//         <header className={!prev || !next ? styles.headerTwo : ""}>
-//           {prev && (
-//             <Link to={`/musicians/${prev.slug}`}>
-//               <span>Previous</span>
-//             </Link>
-//           )}
-//           <h2>{title}</h2>
-//           {next && (
-//             <Link to={`/musicians/${next.slug}`}>
-//               <span>Next</span>
-//             </Link>
-//           )}
-//         </header>
-//         <Img fixed={image.fixed} />
-//       </main>
-//     </HeaderFooterLayout>
-//   )
-// }
+  return (
+    <HeaderFooterLayout>
+      <main className={styles.container}>
+        <header className={!prev || !next ? styles.headerTwo : ""}>
+          {prev && (
+            <Link to={`/post/${prev.slug}`}>
+              <span>Previous</span>
+            </Link>
+          )}
+          <h2>{title}</h2>
+          {next && (
+            <Link to={`/post/${next.slug}`}>
+              <span>Next</span>
+            </Link>
+          )}
+        </header>
+        <Img fixed={image.fixed} />
+      </main>
+    </HeaderFooterLayout>
+  )
+}
 
-// export default MusicianPost
+export default MusicianPost
