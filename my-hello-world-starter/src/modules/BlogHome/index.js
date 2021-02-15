2bbe1b74e69f31aac331680359a2a8bb92616a07
kknezovic <karla.knezovic@gmail.com>
2021-02-15 04:13:33 +0100

    Latest BlogPosts added on Home

--- my-hello-world-starter/src/modules/BlogHome/index.js
+++ my-hello-world-starter/src/modules/BlogHome/index.js
@@ -1,31 +1,68 @@
- import React from "react"
- import styles from "./style.module.css"
- import BlogParagraph from "../../components/BlogParagraph"
- import Jazz from "../../components/Images/Jazz"
- import Piano from "../../components/Images/Piano"
+import React from 'react'
+import {useStaticQuery, graphql, Link} from 'gatsby'
+import Img from 'gatsby-image'
+import styles from './style.module.css'
 
+ 
+const BlogHome = () => {
+    const data = useStaticQuery(graphql`
+    query {
+      myQuery: allContentfulBlogPost(skip: 0, limit: 2) {
+        nodes {
+          description {
+            internal {
+              content
+            }
+          }
+          body {
+            raw
+          }
+          title
+          slug
+          updatedAt
+          image {
+            fixed(width: 360, height: 300) {
+              src
+              srcSet
+              srcSetWebp
+              srcWebp
+              width
+              height
+              base64
+              aspectRatio
+            }
+          }
+        }
+      }
+    }
+  `)
 
- const firstText = `In 1989 saxophonist Loren Schoenberg did a series of 
- interviews with Mel Lewis on the history of jazz 
- drumming.
- These interviews have been talked about and passed 
- around for years, but now, in  the Internet age, we can 
- get our hands on anything, any time.`
-
- const secondText = `Learning to play the piano is a rich and rewarding 
- experience. Some people choose the classical route, 
- learning Beethoven or Chopin. Others opt for 
- famous piano songs from the realm of pop and rock.`
-
- const BlogHome = (activeTab) => (
-    <div className={styles.container}>
-        <h1 className={styles.title}>RECENT BLOG ARTICLES:</h1>
-        <section className={styles.blogContent}>
-            <BlogParagraph image={<Jazz />} title="The History Of Jazz Drums" text={firstText} />
-            <BlogParagraph image={<Piano />} title="Top 10 Popular Piano Songs For Beginners" text={secondText} />
-        </section>
-       
-    </div>
- )
-
+  return (
+<div className={styles.container}>
+ <h1 className={styles.title}>BLOG</h1>
+      <ul className={styles.list}>
+        {data.myQuery.nodes.map(node => {
+          return (
+            <Link to={`/post/${node.slug}`}>
+              <div className={styles.blogParagraph}>
+                  <div className={styles.imgHalf}>
+                    <Img fixed={node.image.fixed} />
+                    <div className={styles.artHalf}>
+                      <article>
+                        <h2>{node.title}</h2>
+                        <p>{node.description.internal.content}</p>
+                        <section>
+                             <button className={styles.button1}>See more</button>
+                        </section>
+                      </article>
+                    </div>
+                  </div>
+              </div>
+            </Link>
+          )
+        })}
+      </ul>
+</div>
+  )
+}
 export default BlogHome
