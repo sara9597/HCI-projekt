7c06af7a0b09fc7027231f673f0f175d571143ef
sara9597 <48886308+sara9597@users.noreply.github.com>
2021-02-24 20:12:51 +0100

    Add files via upload

---
+++ my-hello-world-starter/public/component---src-pages-404-js-da88ccbac1e45b6a7e78.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"ayy... nothing here!"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-da88ccbac1e45b6a7e78.js.map
